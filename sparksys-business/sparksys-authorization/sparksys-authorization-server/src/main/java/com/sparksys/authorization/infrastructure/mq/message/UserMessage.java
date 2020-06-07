f01a3b7e909804c84da29b2cc0cb18b204defc72
zhouxinlei <zhouxinlei298@163.com>
2020-06-07 18:13:04 +0800

    集成kafka

---
+++ sparksys-business/sparksys-authorization/sparksys-authorization-server/src/main/java/com/sparksys/authorization/infrastructure/mq/message/UserMessage.java
@@ -0,0 +1,27 @@
+package com.sparksys.authorization.infrastructure.mq.message;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/**
+ * description：
+ *
+ * @Author zhouxinlei
+ * @Date 2020/6/7 5:09 下午
+ */
+@Data
+@ToString
+public class UserMessage implements Serializable {
+
+    private static final long serialVersionUID = -8895896197064940286L;
+
+
+    public static final String TOPIC = "USER_TOPIC_01";
+
+    /**
+     * 编号
+     */
+    private Integer id;
+}
