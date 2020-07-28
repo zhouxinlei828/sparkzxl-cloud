b0fa1f06af1485fc9378f3b37b7667ec8af0a66a
zhouxinlei <zhouxinlei298@163.com>
2020-07-28 22:33:06 +0800

    circuitbreaker

--- sparksys-gateway/src/main/java/com/sparksys/gateway/GatewayApplication.java
+++ sparksys-gateway/src/main/java/com/sparksys/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package com.sparksys.gateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @SpringBootApplication(scanBasePackages = {"com.sparksys.gateway"})
 @Slf4j
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableFeignClients({"com.sparksys.gateway"})
 public class GatewayApplication {
