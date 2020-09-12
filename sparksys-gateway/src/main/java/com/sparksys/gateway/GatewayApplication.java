a6ff6e078d74528aae979759ef693f9b71f02247
zhouxinlei <zhouxinlei298@163.com>
2020-09-12 12:08:35 +0800

    gateway优化

--- sparksys-gateway/src/main/java/com/sparksys/gateway/GatewayApplication.java
+++ sparksys-gateway/src/main/java/com/sparksys/gateway/GatewayApplication.java
@@ -1,9 +1,7 @@
 package com.sparksys.gateway;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * description: 网关启动类
@@ -12,8 +10,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  * @date 2020-05-24 12:18:18
  */
 @SpringBootApplication(scanBasePackages = {"com.sparksys.gateway"})
-@Slf4j
-@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class GatewayApplication {
 
     public static void main(String[] args) {
