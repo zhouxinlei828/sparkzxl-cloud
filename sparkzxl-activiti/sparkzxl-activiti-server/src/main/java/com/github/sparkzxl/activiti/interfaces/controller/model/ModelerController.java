e1b069b8a3defd33326961bab5734cd70f793b44
zhouxinlei <zhouxinlei298@163.com>
2020-10-02 22:02:48 +0800

    activiti 集成在线流程设计器

--- sparkzxl-activiti/sparkzxl-activiti-server/src/main/java/com/github/sparkzxl/activiti/interfaces/controller/model/ModelerController.java
+++ sparkzxl-activiti/sparkzxl-activiti-server/src/main/java/com/github/sparkzxl/activiti/interfaces/controller/model/ModelerController.java
@@ -13,7 +13,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.activiti.engine.RepositoryService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
