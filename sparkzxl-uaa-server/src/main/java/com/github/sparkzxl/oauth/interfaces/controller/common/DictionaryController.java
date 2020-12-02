7b752e875aa14f86b2e6672e9714decf4d947bb1
zhouxinlei <zhouxinlei298@163.com>
2020-12-02 15:07:15 +0800

    字典维护

--- sparkzxl-uaa-server/src/main/java/com/github/sparkzxl/oauth/interfaces/controller/common/DictionaryController.java
+++ sparkzxl-uaa-server/src/main/java/com/github/sparkzxl/oauth/interfaces/controller/common/DictionaryController.java
@@ -1,9 +1,16 @@
 package com.github.sparkzxl.oauth.interfaces.controller.common;
 
 
+import com.github.sparkzxl.database.base.controller.SuperCacheController;
 import com.github.sparkzxl.log.annotation.WebLog;
+import com.github.sparkzxl.oauth.application.service.ICommonDictionaryService;
+import com.github.sparkzxl.oauth.infrastructure.entity.CommonDictionary;
+import com.github.sparkzxl.oauth.interfaces.dto.dictionary.DictionaryPageDTO;
+import com.github.sparkzxl.oauth.interfaces.dto.dictionary.DictionarySaveDTO;
+import com.github.sparkzxl.oauth.interfaces.dto.dictionary.DictionaryUpdateDTO;
 import com.github.sparkzxl.web.annotation.ResponseResult;
 import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,6 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 @ResponseResult
 @WebLog
 @Api(tags = "字典类型管理")
-public class DictionaryController {
+@RequestMapping("/dictionary")
+public class DictionaryController extends SuperCacheController<ICommonDictionaryService, Long,
+        CommonDictionary, DictionaryPageDTO, DictionarySaveDTO, DictionaryUpdateDTO> {
+
 
 }
