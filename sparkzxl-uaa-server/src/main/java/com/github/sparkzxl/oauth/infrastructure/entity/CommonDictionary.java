b307d2cc64a62d674185a7039b8014a4a4b98d5e
zhouxinlei <zhouxinlei298@163.com>
2020-12-03 18:33:42 +0800

    数据字段格式

--- sparkzxl-uaa-server/src/main/java/com/github/sparkzxl/oauth/infrastructure/entity/CommonDictionary.java
+++ sparkzxl-uaa-server/src/main/java/com/github/sparkzxl/oauth/infrastructure/entity/CommonDictionary.java
@@ -8,8 +8,6 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.util.Date;
-
 import static com.baomidou.mybatisplus.annotation.SqlCondition.LIKE_RIGHT;
 
 /**
