c578bab135a871920dcde94cee17094f650fca51
zhouxinlei <zhouxinlei298@163.com>
2020-12-04 09:15:19 +0800

    activiti 工作流主键自定义生成策略

--- sparkzxl-activiti/sparkzxl-activiti-server/src/main/java/com/github/sparkzxl/activiti/infrastructure/config/SnowFlakeGenerator.java
+++ sparkzxl-activiti/sparkzxl-activiti-server/src/main/java/com/github/sparkzxl/activiti/infrastructure/config/SnowFlakeGenerator.java
@@ -4,6 +4,12 @@ import cn.hutool.core.util.IdUtil;
 import org.activiti.engine.impl.cfg.IdGenerator;
 import org.springframework.stereotype.Component;
 
+/**
+ * description: activiti自定义主键生成策略（雪花）
+ *
+ * @author: zhouxinlei
+ * @date: 2020-12-04 09:14:45
+*/
 @Component
 public class SnowFlakeGenerator implements IdGenerator {
 
