00b477ce6a52617510d0f99f8bd6604a461f3bce
zhouxinlei <zhouxinlei298@163.com>
2020-12-23 11:18:14 +0800

    修改activiti

--- sparkzxl-uaa-server/src/main/java/com/github/sparkzxl/oauth/interfaces/controller/common/AreaController.java
+++ sparkzxl-uaa-server/src/main/java/com/github/sparkzxl/oauth/interfaces/controller/common/AreaController.java
@@ -2,7 +2,6 @@ package com.github.sparkzxl.oauth.interfaces.controller.common;
 
 
 import com.github.sparkzxl.database.base.controller.SuperCacheController;
-import com.github.sparkzxl.log.annotation.WebLog;
 import com.github.sparkzxl.oauth.application.service.ICommonAreaService;
 import com.github.sparkzxl.oauth.infrastructure.entity.CommonArea;
 import com.github.sparkzxl.oauth.interfaces.dto.area.AreaPageDTO;
@@ -26,7 +25,6 @@ import java.util.List;
  */
 @RestController
 @ResponseResult
-@WebLog
 @Api(tags = "地区管理")
 @RequestMapping("/common/area")
 public class AreaController extends SuperCacheController<ICommonAreaService, Long,
