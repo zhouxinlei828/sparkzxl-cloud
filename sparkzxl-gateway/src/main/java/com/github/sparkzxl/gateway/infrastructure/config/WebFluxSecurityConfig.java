a6693b3877f111de97e89cc25bd9a14217ad7eff
zhouxinlei <zhouxinlei298@163.com>
2021-01-30 14:18:52 +0800

    授权登录

--- sparkzxl-gateway/src/main/java/com/github/sparkzxl/gateway/infrastructure/config/WebFluxSecurityConfig.java
+++ sparkzxl-gateway/src/main/java/com/github/sparkzxl/gateway/infrastructure/config/WebFluxSecurityConfig.java
@@ -4,7 +4,6 @@ import com.github.sparkzxl.gateway.infrastructure.components.MyServerAuthenticat
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
