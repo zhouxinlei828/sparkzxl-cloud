8e009b0864c159023ed56c0954c4e6f8713db3f0
zhouxinlei <zhouxinlei298@163.com>
2021-01-30 14:15:35 +0800

    授权登录

--- sparkzxl-gateway/src/main/java/com/github/sparkzxl/gateway/infrastructure/components/MyServerAuthenticationSuccessHandler.java
+++ sparkzxl-gateway/src/main/java/com/github/sparkzxl/gateway/infrastructure/components/MyServerAuthenticationSuccessHandler.java
@@ -6,10 +6,6 @@ import com.github.sparkzxl.gateway.infrastructure.handler.AuthenticationTokenHan
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.server.DefaultServerRedirectStrategy;
 import org.springframework.security.web.server.ServerRedirectStrategy;
 import org.springframework.security.web.server.WebFilterExchange;
@@ -37,8 +33,6 @@ public class MyServerAuthenticationSuccessHandler implements ServerAuthenticatio
 
     @Value("${application.frontend_url}")
     private String defaultLoginSuccessUrl;
-    @Autowired
-    private ReactiveOAuth2AuthorizedClientManager authorizedClientManager;
 
     @Override
     public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
@@ -53,15 +47,4 @@ public class MyServerAuthenticationSuccessHandler implements ServerAuthenticatio
         return this.redirectStrategy
                 .sendRedirect(webFilterExchange.getExchange(), url);
     }
-
-
-    private Mono<OAuth2AuthorizedClient> authorizeClient(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
-        final String clientRegistrationId = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
-        return authorizedClientManager.authorize(createOauth2AuthorizeRequest(clientRegistrationId, oAuth2AuthenticationToken));
-    }
-
-    private OAuth2AuthorizeRequest createOauth2AuthorizeRequest(String clientRegistrationId, Authentication principal) {
-        return OAuth2AuthorizeRequest.withClientRegistrationId(clientRegistrationId).principal(principal).build();
-    }
-
 }
