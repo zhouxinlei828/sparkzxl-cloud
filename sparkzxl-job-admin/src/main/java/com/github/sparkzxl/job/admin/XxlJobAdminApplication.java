1eda15da0bfd8ac571c2158ef7b02b7be59f5baf
zhouxinlei <zhouxinlei298@163.com>
2021-03-15 21:37:12 +0800

    代码升级

--- sparkzxl-job-admin/src/main/java/com/github/sparkzxl/job/admin/XxlJobAdminApplication.java
+++ sparkzxl-job-admin/src/main/java/com/github/sparkzxl/job/admin/XxlJobAdminApplication.java
@@ -1,5 +1,6 @@
 package com.github.sparkzxl.job.admin;
 
+import com.github.sparkzxl.boot.SparkBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author xuxueli 2018-10-28 00:38:13
  */
 @SpringBootApplication
-public class XxlJobAdminApplication {
+public class XxlJobAdminApplication extends SparkBootApplication {
 
 	public static void main(String[] args) {
         SpringApplication.run(XxlJobAdminApplication.class, args);
